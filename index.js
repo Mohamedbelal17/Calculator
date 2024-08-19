7cfed1f72120c3878ac8216bfef24ceb6924eda7
Mohamedbelal17 <Mohamed.adBelal.69@gmail.com>
2024-08-20 01:21:16 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+function getValue(value) {
+  let el = document.getElementById("result");
+  el.value += value;
+}
+
+function getResult() {
+  let el = document.getElementById("result");
+  let str = el.value;
+  try {
+    if (/[\+\-\*\/]$/.test(str)) {
+      throw new Error("Incomplete Calculation");
+    }
+    let result = eval(str);
+    el.value = result;
+  } catch (error) {
+    el.value = "Error";
+  }
+}
+// try {
+//     if (/[\+\-\*\/]$/.test(str)) {
+//       throw new Error("Incomplete calculation");
+//     }
+//     let result = eval(str);
+//     el.value = result;
+//   } catch (error) {
+//     el.value = "Error";
+//   }
+
+function Rest() {
+  document.getElementById("result").value = "";
+}
+function Delete() {
+  let el = document.getElementById("result").value;
+  document.getElementById("result").value = el.slice(0, -1);
+}
